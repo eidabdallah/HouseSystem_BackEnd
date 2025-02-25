8009207f4ca05494329fe2a02206da0b9c62f40e
eidabdallah <eidabdallah971@gmail.com>
2025-02-25 19:44:27 +0200

    create admin acount

--- src/modules/app.router.js
+++ src/modules/app.router.js
@@ -1,12 +1,13 @@
 import { connectDB } from '../../DB/connection.js';
 import cors from 'cors';
 import { globalhandleError } from '../utils/AppError.js';
+import { createAdmin } from '../utils/createAdmin.js';
 
 export const initApp = async (app, express) => {
     await connectDB();
+    await createAdmin();
     app.use(cors());
     app.use(express.json());
-
     app.get('/', (req, res) => {
         return res.status(200).json({ message: 'Welcome to the House System app' });
     });
---
+++ src/utils/createAdmin.js
@@ -0,0 +1,24 @@
+import userModel from '../../DB/model/user.model.js';
+import bcrypt from 'bcryptjs';
+
+export const createAdmin = async () => {
+    try {
+        const adminExists = await userModel.findOne({ where: { role: 'Admin' } });
+        if (!adminExists) {
+            const adminPassword = bcrypt.hashSync(process.env.AdminPassword, parseInt(process.env.SALTROUND));
+
+            await userModel.create({
+                userName: "Admin",
+                email: process.env.AdminEmail,
+                password: adminPassword,
+                phoneNumber: "0592306697",
+                role: "Admin",
+                status: "Active",
+                confirmEmail: true,
+                gender: "Male",
+            });
+        }
+    } catch (error) {
+        console.error(error);
+    }
+};
