d8087b96d757f28e4807f23400f3a380a05fc325
eidabdallah <eidabdallah971@gmail.com>
2025-02-25 19:36:01 +0200

    create global error handling and async handling

--- src/modules/app.router.js
+++ src/modules/app.router.js
@@ -1,5 +1,6 @@
 import { connectDB } from '../../DB/connection.js';
 import cors from 'cors';
+import { globalhandleError } from '../utils/AppError.js';
 
 export const initApp = async (app, express) => {
     await connectDB();
@@ -9,6 +10,7 @@ export const initApp = async (app, express) => {
     app.get('/', (req, res) => {
         return res.status(200).json({ message: 'Welcome to the House System app' });
     });
+    app.use(globalhandleError);
 
     app.use('*', (req, res) => {
         return res.status(404).json({ message: 'Page Not Found' });
